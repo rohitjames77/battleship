e0e2a195fc4058a749690ae9f590b70db699da26
rohit <rohitjames77@outlook.com>
2025-02-22 07:28:47 +0530

    Add test for hit method to return hits

--- src/testing.test.js
+++ src/testing.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import sum from './index'; 
+import {carrier} from './ships'
 
-test('Ship class object to inherit ', () => {
-  expect().toBe(3);
+
+test('carrier object to have gridLength property ', () => {
+  expect(carrier).toHaveProperty('gridLength');
 });
 
+test('gridLength to be a number', ()=>{
+  expect(typeof carrier.gridLength).toBe('number');
+})
+test('carrier object should have a method called hit', ()=>{
+  expect(carrier).toHaveProperty('hit')
+})
+test('hit function should return number of hits carrier have received if coordinates matches ',()=>{
+  expect(carrier.hit('a2','a2')).toEqual(1);
+})
