fadb79a2680fb29ac0948e37638a50492081b894
rohit <rohitjames77@outlook.com>
2025-02-22 07:30:03 +0530

    Add hit method to carrier of new ship class to return hits

--- src/ships.js
+++ src/ships.js
@@ -1,8 +1,21 @@
- class Ship{
-    constructor(length){
-this.length = length;
+class Ship {
+  constructor() {
+    this.gridLength = 5;
+  }
+  hit(player1Coordinates, player2Coordinates) {
+    let hits = 0;
+    let missed = 0;
+    if (player1Coordinates === player2Coordinates) {
+      hits++;
+      console.log(hits);
+      return hits;
+    } else if (player1Coordinates !== player2Coordinates) {
+      console.log("Missed");
+      missed++;
+      return missed;
     }
-    hit(){
-        
-    }
- }
+  }
+  isSunk() {}
+}
+const carrier = new Ship();
+export { carrier };
