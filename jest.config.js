ea61e54c1244c3121fae5413c4fe7f2aec6b26ab
Rohit <rohitjames77@outlook.com>
2025-03-19 22:44:35 +0530

    Change jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    moduleNameMapper: {
-      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-        '<rootDir>/__mocks__/fileMock.js',
-      '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-    },
-  };
+  testEnvironment: 'node', // Use 'jsdom' for browser-like environment
+  moduleFileExtensions: ['js'],
+  transform: {
+    '^.+\\.js$': 'babel-jest', // Use Babel for transforming JavaScript
+  },
+};
